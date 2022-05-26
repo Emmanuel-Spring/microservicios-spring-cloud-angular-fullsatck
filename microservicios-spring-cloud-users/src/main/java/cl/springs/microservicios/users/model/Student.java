3e03d826bcacaf69886c0ea1b8e8b3bc90be802e
Emmanuel-Spring <e.nieto@alumnos.santotomas.cl>
2022-05-26 04:45:02 -0400

    first commit

--- microservicios-spring-cloud-users/src/main/java/cl/springs/microservicios/users/model/Student.java
+++ microservicios-spring-cloud-users/src/main/java/cl/springs/microservicios/users/model/Student.java
@@ -1,6 +1,86 @@
 package cl.springs.microservicios.users.model;
 
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "students")
 public class Student {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
 
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    @Column(name = "create_at")
+    private Date createAt;
+
+    @PrePersist
+    public void prePersist() {
+        this.createAt = new Date();
+    }
+
+    public Student() {
+    }
+
+    public Student(String firstName, String lastName, String email, Date createAt) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.createAt = createAt;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(Date createAt) {
+        this.createAt = createAt;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", createAt=" + createAt +
+                '}';
+    }
 }
